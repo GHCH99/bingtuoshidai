3f62e078c64a0dd6ac417210622631cf9f3d177f
GHCH99 <GAO20011215@outlook.com>
2025-03-23 22:43:07 +0800

    1.2.3完整版

--- design-spark-genius-main/design-spark-genius-main/vite.config.ts
+++ design-spark-genius-main/design-spark-genius-main/vite.config.ts
@@ -3,15 +3,32 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "",
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        format: "iife",
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]"
+      }
+    }
+  },
+  server: {
+    host: "::",
+    port: 8080,
+  },
   plugins: [
     react(),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
   resolve: {
-    alias: {
+      alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
